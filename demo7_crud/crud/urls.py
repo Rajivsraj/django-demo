63325d25fef7c89dcf8c4839ab1f8f2641cfdcae
Rajiv <rajivsraj78@gmail.com>
2023-05-02 22:11:52 +0530

    crud complete

--- demo7_crud/crud/urls.py
+++ demo7_crud/crud/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path("", views.add_student, name="addStu"),
     path("stu-list", views.list_student, name="stuList"),
     path("delete-stu/<int:stuid>", views.delete_student, name="stuDelete"),
+    path("edit-stu/<stu_id>", views.edit_stu_view, name="name_edit_stu_view"),
+    path("update-stu/", views.update_stu, name="name_update_stu"),
 ]
--- demo7_crud/crud/views.py
+++ demo7_crud/crud/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import StudentDetails
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 
 
 # Create your views here.
@@ -18,8 +18,8 @@ def add_student(request):
         data.password = request.POST.get("password")
         data.email = request.POST.get("email")
         data.phone = request.POST.get("phone")
-        if msg["error"]:
-           pass
+        if len(msg) >= 1:
+           msg["status"] = "cant be saved"
         else:
             msg["status"] = "data Saved"
             data.save()
@@ -37,3 +37,27 @@ def delete_student(request, stuid):
     x = StudentDetails.objects.get(pk=stu_id)
     x.delete()
     return HttpResponseRedirect("/stu-list")
+
+
+def edit_stu_view(request, stu_id):
+    data = {}
+    data["status"] = ""
+    edit = StudentDetails.objects.filter(id=stu_id).first()
+    if edit is None:
+        data["status"] = "Record not found"
+    else:
+        data["single_rec"] = edit
+    return render(request, "crud/edit-stu.html", context={"data": data})
+
+
+def update_stu(request):
+    stu_id = request.POST.get("stu_id")
+    updt = StudentDetails.objects.filter(id=stu_id).first()
+    updt.fname = request.POST.get("fname")
+    updt.lname = request.POST.get("lname")
+    updt.email = request.POST.get("emali")
+    updt.password = request.POST.get("password")
+    updt.phone = request.POST.get("phone")
+    updt.save()
+    return redirect("stuList")
+
