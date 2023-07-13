9b59a537a9301a59cf05c7d3d176253b0f384ceb
Rajiv <rajivsraj78@gmail.com>
2023-07-13 07:56:16 +0530

    Ankit changes

--- Demo14_Crud/crud/views.py
+++ Demo14_Crud/crud/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-
+from .models import StudentDetails
 
 # Create your views here.
 def show(request):
+    data={}
+    if request.method == "POST":
+      data =  StudentDetails(request.POST)
+      data.name = request.POST("name")
+      data.email = request.POST("email")
+      data.password = request.POST("password")
+      data.phone = request.POST("phone")
+
+      data.save()
+
     return render(request, "crud/action.html")
 
