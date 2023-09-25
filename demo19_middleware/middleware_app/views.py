541aac108f8b8c435539e0439aada7902c1a8ba6
Rajiv <rajivsraj78@gmail.com>
2023-09-25 08:40:17 +0530

    class middleware

--- demo19_middleware/middleware_app/views.py
+++ demo19_middleware/middleware_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
-def test_middleware_fun(request):
-    print("This is view fun")
+def test_middleware_fun(request, stuid):
+    print("This is view fun", stuid)
     return HttpResponse("<h1> Hello </h1>")
