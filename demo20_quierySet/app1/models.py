d17f2696a1c9a5871a6c51a78cd00075c20f9082
Abhishek <abhicrick1@gmail.com>
2023-09-30 11:45:17 +0530

    QeurySet Custom Database Connectivity

--- demo20_quierySet/app1/models.py
+++ demo20_quierySet/app1/models.py
@@ -7,3 +7,20 @@ class Employee(models.Model):
     name = models.CharField(max_length=50)
     salary = models.IntegerField()
     city = models.CharField(max_length=50, default=None)
+    date = models.DateField()
+    
+class Student(models.Model):
+    
+    passed = "Pass"
+    Failed = "Fail"
+    
+    result = {
+        (passed , "Pass"),
+        (Failed , "Fail")
+    }
+    
+    stu_id = models.IntegerField(unique=True, null=False)
+    name = models.CharField(max_length=50)
+    result = models.CharField(max_length=50, choices=result)
+    city = models.CharField(max_length=50, default=None)
+    date = models.DateField()
--- demo20_quierySet/demo20_quierySet/settings.py
+++ demo20_quierySet/demo20_quierySet/settings.py
@@ -76,13 +76,13 @@ WSGI_APPLICATION = 'demo20_quierySet.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        # "ENGINE": "django.db.backends.mysql",
-        # "NAME": "komal",
-        # "USER": "root",
-        # "HOST": "localhost",
-        # "PASSWORD": ""
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "queryset_demo",
+        "USER": "root",
+        "HOST": "localhost",
+        "PASSWORD": ""
     }
 }
 
