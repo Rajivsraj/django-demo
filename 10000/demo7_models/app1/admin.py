58597228e4a00cb0e513f9cb1b2f3b0369f79e97
Rajiv <rajivsraj78@gmail.com>
2023-10-25 20:47:38 +0530

    Model

---
+++ 10000/demo7_models/app1/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import Student, Studetn2
+
+# Register your models here.
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ["name", "city", "age", "phone"]
+
+
+@admin.register(Studetn2)
+class Student2Admin(admin.ModelAdmin):
+    # list_display = ["name", "city", "city2", "city3"]
+    list_display = ["name", "city2", "city3"]
+    exclude = ["city3"]
+
+
